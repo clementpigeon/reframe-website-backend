b01fc47f8a63c914dfd477f82cdb36ad10c66d58
Clément Pigeon <clement.pigeon@samedi.de>
2022-08-18 15:48:02 +0200

    Add credentials for Plesk MariaDB database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,40 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+module.exports = ({ env }) => {
+  if (false) {
+    return ({
+      connection: {
+        client: 'mysql',
+        connection: {
+          "host": "localhost",
+          "port": 3306,
+          "database": "strapi",
+          "username": "strapi",
+          "password": "Cq6u_n454", // TODO update
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
       },
+    });
+  }
+
+  return ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  });
+}
